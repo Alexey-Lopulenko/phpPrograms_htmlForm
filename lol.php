754187883a5ee5077e25a0f880b8e385518c3601
Alexey-Lopulenko <alexlojlua@gmail.com>
2019-04-05 21:21:23 +0300

    lol.php

--- lol.php
+++ lol.php
@@ -1,13 +1,4 @@
 <?php 
-echo "Privet!!!<br>";
-$text = "7.25";
-echo $text,"<br>";
-$number  = (double)$text;
-
-(int)$res = $number * 100;
-$res2 = $res % 100;
-echo $res2;
-
-echo "<br>",end($arr);
+echo "Hello World!!!<br>";
 
 ?>
